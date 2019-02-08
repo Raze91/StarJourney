8fdfb89e82bd44376b7d8263deb2f5325b0ca4fd
Sami Hattab <hattab_sami@yahoo.fr>
2019-02-08 12:52:01 +0100

    Legere factorisation du code

--- js/game.js
+++ js/game.js
@@ -41,7 +41,7 @@ WorkShop.game = {
         // Asteroid 1
         loader.load('./assets/source/asteroid.fbx', function (object) {
             object.scale.set(0.03, 0.03, 0.03)
-            object.position.set(0,70,0);
+            object.position.set(0, 70, 0);
             WorkShop.game.asteroid = object;
             WorkShop.gfx_engine.scene.add(WorkShop.game.asteroid);
         });
@@ -49,7 +49,7 @@ WorkShop.game = {
         // Asteroid 2
         loader.load('./assets/source/asteroid.fbx', function (object) {
             object.scale.set(0.03, 0.03, 0.03);
-            object.position.set(40,70,0);
+            object.position.set(40, 70, 0);
             WorkShop.game.asteroid_2 = object;
             WorkShop.gfx_engine.scene.add(WorkShop.game.asteroid_2);
         });
@@ -57,7 +57,7 @@ WorkShop.game = {
         // Asteroid 3
         loader.load('./assets/source/asteroid.fbx', function (object) {
             object.scale.set(0.03, 0.03, 0.03);
-            object.position.set(70,70,0);
+            object.position.set(70, 70, 0);
             WorkShop.game.asteroid_3 = object;
             WorkShop.gfx_engine.scene.add(WorkShop.game.asteroid_3);
         });
@@ -93,28 +93,13 @@ WorkShop.game = {
                 WorkShop.game.moveRight = true;
                 break;
             case 27: // Echap
-                if(WorkShop.GameOver == false){
+                if (WorkShop.GameOver == false) {
                     WorkShop.setPause();
                     console.log('echap');
                 }
                 break;
         }
     },
-    onKeyUp: function (event) {
-        switch (event.keyCode) {
-            case 37: // left
-            case 65: // a
-                //WorkShop.game.moveLeft = false;
-                //console.log('ne vas plus a gauche');
-                break;
-            case 39: // right
-            case 68: // d
-                //WorkShop.game.moveRight = false;
-                //console.log('ne vas plus a droite');
-                break;
-        }
-    },
-
 
     getRandomArbitrary: function (min, max) {
         return Math.random() * (max - min) + min;
@@ -124,32 +109,25 @@ WorkShop.game = {
 
         WorkShop.IsGameOver();
 
-         this.player.score_div.innerText = 'Score : ' + this.player.score;
+        this.player.score_div.innerText = 'Score : ' + this.player.score;
 
-         this.player.lives_div.innerText = 'Vies : ' + this.player.lives;
+        this.player.lives_div.innerText = 'Vies : ' + this.player.lives;
 
-         if(WorkShop.game.moveLeft == true){
-             this.ship.translateZ(-1);
-         }
+        if (WorkShop.game.moveLeft == true) {
+            this.ship.translateZ(-1);
+        }
 
-         if(WorkShop.game.moveRight == true){
-             this.ship.translateZ(1);
-         }
+        if (WorkShop.game.moveRight == true) {
+            this.ship.translateZ(1);
+        }
 
-         if(this.ship.position.x <= -70){
-             WorkShop.game.moveLeft = false;
-         }
+        if (this.ship.position.x <= -70) {
+            WorkShop.game.moveLeft = false;
+        }
 
-         if(this.ship.position.x >= 70){
-             WorkShop.game.moveRight = false;
-         }
-
-        //  if (this.ship != null && this.ship.position.x > 0 && this.moveRight != true) {
-        //      this.ship.translateZ(-1)
-        //  }
-        //  if (this.ship != null && this.ship.position.x < 0 && this.moveLeft != true) {
-        //      this.ship.translateZ(1)
-        //  }
+        if (this.ship.position.x >= 70) {
+            WorkShop.game.moveRight = false;
+        }
 
         if (this.asteroid != null && this.asteroid.position.y > -50) {
             this.asteroid.translateY(-2);
@@ -161,51 +139,51 @@ WorkShop.game = {
             this.asteroid_3.translateY(-2);
         }
 
-         if (this.ship != null && 
-             this.asteroid != null && 
-             this.asteroid.position.x - 6 < this.ship.position.x + 5 && 
-             this.asteroid.position.x + 6 > this.ship.position.x - 5 && 
-             this.asteroid.position.y - 6 < this.ship.position.y + 9 && 
-             this.asteroid.position.y + 6 > this.ship.position.y - 9) {
-             this.player.lives -= 1;
-             this.asteroid.position.set(this.getRandomArbitrary(-70, 70), this.getRandomArbitrary(50, 70), 0);
-         }
-         if (this.ship != null && 
-             this.asteroid_2 != null && 
-             this.asteroid_2.position.x - 6 < this.ship.position.x + 5 && 
-             this.asteroid_2.position.x + 6 > this.ship.position.x - 5 && 
-             this.asteroid_2.position.y + 6 < this.ship.position.y + 9 && 
-             this.asteroid_2.position.y + 6 > this.ship.position.y - 9) {
-             this.player.lives -= 1;
-             this.asteroid_2.position.set(this.getRandomArbitrary(-70, 70), this.getRandomArbitrary(50, 70), 0)
-         }
-         if (this.ship != null && 
-             this.asteroid_3 != null && 
-             this.asteroid_3.position.x - 6 < this.ship.position.x + 5 && 
-             this.asteroid_3.position.x + 6 > this.ship.position.x - 5 && 
-             this.asteroid_3.position.y + 6 < this.ship.position.y + 9 && 
-             this.asteroid_3.position.y + 6 > this.ship.position.y - 9) {
-             this.player.lives -= 1;
-             this.asteroid_3.position.set(this.getRandomArbitrary(-70, 70), this.getRandomArbitrary(50, 70), 0)
-         }
+        if (this.ship != null &&
+            this.asteroid != null &&
+            this.asteroid.position.x - 6 < this.ship.position.x + 5 &&
+            this.asteroid.position.x + 6 > this.ship.position.x - 5 &&
+            this.asteroid.position.y - 6 < this.ship.position.y + 9 &&
+            this.asteroid.position.y + 6 > this.ship.position.y - 9) {
+            this.player.lives -= 1;
+            this.asteroid.position.set(this.getRandomArbitrary(-70, 70), this.getRandomArbitrary(50, 70), 0);
+        }
+        if (this.ship != null &&
+            this.asteroid_2 != null &&
+            this.asteroid_2.position.x - 6 < this.ship.position.x + 5 &&
+            this.asteroid_2.position.x + 6 > this.ship.position.x - 5 &&
+            this.asteroid_2.position.y + 6 < this.ship.position.y + 9 &&
+            this.asteroid_2.position.y + 6 > this.ship.position.y - 9) {
+            this.player.lives -= 1;
+            this.asteroid_2.position.set(this.getRandomArbitrary(-70, 70), this.getRandomArbitrary(50, 70), 0)
+        }
+        if (this.ship != null &&
+            this.asteroid_3 != null &&
+            this.asteroid_3.position.x - 6 < this.ship.position.x + 5 &&
+            this.asteroid_3.position.x + 6 > this.ship.position.x - 5 &&
+            this.asteroid_3.position.y + 6 < this.ship.position.y + 9 &&
+            this.asteroid_3.position.y + 6 > this.ship.position.y - 9) {
+            this.player.lives -= 1;
+            this.asteroid_3.position.set(this.getRandomArbitrary(-70, 70), this.getRandomArbitrary(50, 70), 0)
+        }
 
-         if (this.asteroid != null && this.asteroid.position.y <= -50) {
-             this.player.score += 10;
-             this.asteroid.position.set(this.getRandomArbitrary(-70, 70), this.getRandomArbitrary(50, 70), 0);
-         }
-         if (this.asteroid_2 != null && this.asteroid_2.position.y <= -50) {
-             this.player.score += 10;
-             this.asteroid_2.position.set(this.getRandomArbitrary(-70, 70), this.getRandomArbitrary(50, 70), 0)
-         }
-         if (this.asteroid_3 != null && this.asteroid_3.position.y <= -50) {
-             this.player.score += 10;
-             this.asteroid_3.position.set(this.getRandomArbitrary(-70, 70), this.getRandomArbitrary(50, 70), 0)
-         }
+        if (this.asteroid != null && this.asteroid.position.y <= -50) {
+            this.player.score += 10;
+            this.asteroid.position.set(this.getRandomArbitrary(-70, 70), this.getRandomArbitrary(50, 70), 0);
+        }
+        if (this.asteroid_2 != null && this.asteroid_2.position.y <= -50) {
+            this.player.score += 10;
+            this.asteroid_2.position.set(this.getRandomArbitrary(-70, 70), this.getRandomArbitrary(50, 70), 0)
+        }
+        if (this.asteroid_3 != null && this.asteroid_3.position.y <= -50) {
+            this.player.score += 10;
+            this.asteroid_3.position.set(this.getRandomArbitrary(-70, 70), this.getRandomArbitrary(50, 70), 0)
+        }
 
-         if(this.player.lives <= 0){
-             //WorkShop.pause = true
-             WorkShop.GameOver = true;
-         }
+        if (this.player.lives <= 0) {
+            //WorkShop.pause = true
+            WorkShop.GameOver = true;
+        }
 
     },
 
